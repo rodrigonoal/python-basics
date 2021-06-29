41f70b4f53bef0b305d16c0d7c2f5d79cd24da1c
RodrigoNoal <80356919+rodrigonoal@users.noreply.github.com>
2021-06-29 13:58:47 -0300

    Update first_py_library.py
    lib pywhatkit

--- first_py_library.py
+++ first_py_library.py
@@ -1,5 +1,5 @@
-#(Powershell) pip install pywhatkit
+#(Powershell) pip  install pywhatkit
 
 import pywhatkit as kit
 
-kit.sendwhatmsg("+55#########", "Essa é uma library do python que envia msg de whats", 13,46) #hora, minuto
+kit.sendwhatmsg("+55#########", "Essa é uma library do python que envia msg de whats", 13,46) #hora, minuto 
