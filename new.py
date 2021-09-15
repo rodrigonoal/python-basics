6df4b57c1b4c67f9479ae4d99a23f5c026d77e01
RodrigoNoal <80356919+rodrigonoal@users.noreply.github.com>
2021-09-14 21:09:27 -0300

    Add files via upload

---
+++ new.py
@@ -0,0 +1,28 @@
+# funções:
+
+def soma(a,b):
+    return a + b;
+
+def preco(alberto, centavos):
+    return "R$ " + alberto + "," + centavos
+
+
+a = "String" #string
+
+b = ["Jão Cleber", "Maria", 3.5] #list
+
+c = 3 #integer
+
+d = 4.5 #float
+
+e = False #booleanos
+
+if 5 == 5:
+    print("Sâo iguais!") # condicional
+
+
+# Operações matemáticas:
+# + - / * %
+
+# Operações lógicas:
+# > < ==
